be79ed44f4e4c12c0aa71da761378228d2902cc3
Jing Zhang <jingzhang@Jings-MacBook-Pro.local>
2016-07-09 21:55:29 -0400

    0.1.1

--- SmiR/R/conserved.R
+++ SmiR/R/conserved.R
@@ -1,6 +1,9 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Written by Jing Zhang, Ph.D. Please direct questions to jzhangcad@gmail.com.
-For novel miRNA discovery, be sure to copy training.smir and test.smir files to your working directory.")
+  packageStartupMessage("
+Written by Jing Zhang, Ph.D. Please direct questions to jzhangcad@gmail.com.
+
+For novel miRNA discovery, be sure to copy training.smir and test.smir files
+    to your working directory, as well as to install pcregrep command.")
   return(TRUE)
 }
 
