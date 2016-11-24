0db563a2b2b5b10ba1c1117e844c9d730943b58a
Jing Zhang <jzhangc@Jings-rMBP.local>
2016-11-23 22:40:08 -0500

    0.1.3 name change

--- RBioMIR/R/conserved.R
+++ RBioMIR/R/conserved.R
@@ -2,7 +2,7 @@
   packageStartupMessage("
 Written by Jing Zhang, Ph.D. Please direct questions to jzhangcad@gmail.com.
 
-For novel miRNA discovery, be sure to copy training.smir and test.smir files
+(test only) For novel miRNA discovery, be sure to copy training.smir and test.smir files
     to your working directory, and install pcregrep and parallel commands.")
   return(TRUE)
 }
