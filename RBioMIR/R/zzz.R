63ed1d0643eeab81ce29aebaf6c219946930d761
Jing Zhang <jingzhang@Jings-iMac-mid-2017.local>
2018-09-12 22:30:48 -0400

    0.1.8a1

--- RBioMIR/R/zzz.R
+++ RBioMIR/R/zzz.R
@@ -1,8 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("
-                        Written by Jing Zhang, Ph.D. Please direct questions to jzhangcad@gmail.com.
-
-                        (test only) For novel miRNA discovery, be sure to copy training.smir and test.smir files
-                        to your working directory, and install pcregrep and parallel commands.")
+  packageStartupMessage("Written by Jing Zhang, Ph.D. Please direct questions to jzhangcad@gmail.com.
+  (test only) For novel miRNA discovery, be sure to copy training.smir and test.smir files to your working directory,
+  and install pcregrep and parallel commands.")
   return(TRUE)
 }
